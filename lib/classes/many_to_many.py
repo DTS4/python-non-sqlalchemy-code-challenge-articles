00d50df7f17ae7ab500c2cea983fec3c580e5806
Keith <keith.githinji@student.moringaschool.com>
2024-12-06 12:58:45 +0300

    Added article in Authour and  magazine

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -6,19 +6,31 @@ class Article:
         
 class Author:
     def __init__(self, name):
-        self.name = name
+        if not isinstance(name,str) or len(name) == 0:
+            raise ValueError("Name must be a non-empty string.")
+        self._name = name
+        self._articles = []
+
+    def name(self):
+        return self._name
 
     def articles(self):
-        pass
+        return self._articles
 
     def magazines(self):
-        pass
+        return list({article.magazine for article in self._articles})
 
     def add_article(self, magazine, title):
-        pass
+        if not isinstance(magazine,Magazine):
+            raise ValueError("Magazine must be an instance of the Magazine class.")
+        new_article=Article(self, magazine, title)
+        self._articles.append(new_article)
+        magazine._article.append(new_article)
 
     def topic_areas(self):
-        pass
+        if not self._articles:
+            return None
+        return list({article.magazine.category for article in self._articles})
 
 class Magazine:
     def __init__(self, name, category):
